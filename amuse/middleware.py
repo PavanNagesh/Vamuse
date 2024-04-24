d28f6fa8c23d761b40aaca35330f73a13e557e53
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-25 05:26:37 +0530

    Update middleware.py

--- amuse/middleware.py
+++ amuse/middleware.py
@@ -10,17 +10,28 @@ class BruteForceMiddleware:
     def __call__(self, request):
         if request.user.is_authenticated:
             request.session.setdefault('login_attempts', 0)
+            request.session.setdefault('last_login_attempt', None)
 
         response = self.get_response(request)
 
         if request.user.is_authenticated and response.status_code == 200:
             login_attempts = request.session.get('login_attempts', 0)
+            last_login_attempt = request.session.get('last_login_attempt')
             if login_attempts >= settings.MAX_LOGIN_ATTEMPTS:
-                logout(request)
-                messages.error(request, "Your account is temporarily locked. Please try again later.")
+                if last_login_attempt is not None and \
+                        timezone.now() < last_login_attempt + timezone.timedelta(seconds=settings.LOCKOUT_TIME):
+                    # Still within lockout time, logout user
+                    logout(request)
+                    messages.error(request, "Your account is temporarily locked. Please try again later.")
+                else:
+                    # Reset login attempts and last login attempt time
+                    request.session['login_attempts'] = 0
+                    request.session['last_login_attempt'] = None
             elif 'invalid_login' in request.POST:
                 request.session['login_attempts'] = login_attempts + 1
+                request.session['last_login_attempt'] = timezone.now()
             else:
                 request.session['login_attempts'] = 0
+                request.session['last_login_attempt'] = None
 
         return response
