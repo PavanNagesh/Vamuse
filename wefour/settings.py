f7c2c6e78bbf87e2d9a6560779af9fbcfa057068
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-25 04:06:43 +0530

    Update settings.py

--- wefour/settings.py
+++ wefour/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.auth.backends import ModelBackend
+from two_factor.auth_backends import OTPAuthenticationBackend
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
