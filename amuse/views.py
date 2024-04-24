5d2987a462304293d1c531f8cdc0e7a116b2229f
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-25 03:34:41 +0530

    Update views.py

--- amuse/views.py
+++ amuse/views.py
@@ -80,7 +80,7 @@ def user_login(request):
             send_mail(
                 'Your OTP for Login Verification',
                 f'Your OTP is: {otp}',
-                'sender@example.com',  # Replace with your sender email
+                'pavannagesh681@gmail.com',  # Replace with your sender email
                 [user.email],  # Send OTP to user's email
                 fail_silently=False,
             )
