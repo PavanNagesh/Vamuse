eb50837fcd468099ae39f4deb84beb919314c980
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-27 19:56:20 +0530

    Update models.py

--- amuse/models.py
+++ amuse/models.py
@@ -1,44 +1,33 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+from django.utils import timezone
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, unique=True)  # Adding username field
+    username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
+    failed_login_attempts = models.IntegerField(default=0)
+    locked_out_until = models.DateTimeField(null=True, blank=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.username
-    
-    pass
 
+    def increment_failed_login_attempts(self):
+        self.failed_login_attempts += 1
+        if self.failed_login_attempts >= 5:
+            self.locked_out_until = timezone.now() + timezone.timedelta(minutes=1)
+            self.failed_login_attempts = 0
+        self.save()
 
-    # Define a custom intermediary model for the groups field
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name='groups',
-        blank=True,
-        related_name='user_set_custom',
-        related_query_name='user_custom',
-        through='UserGroup',
-    )
+    def reset_failed_login_attempts(self):
+        self.failed_login_attempts = 0
+        self.locked_out_until = None
+        self.save()
 
-    # Define a custom intermediary model for the user_permissions field
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name='user permissions',
-        blank=True,
-        related_name='user_set_custom',
-        related_query_name='user_custom',
-        through='UserPermission',
-    )
-
-# Custom intermediary model for the groups field
 class UserGroup(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
-
-# Custom intermediary model for the user_permissions field
 class UserPermission(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
