858bbabe09c02454ebf31bc08ea5caeb7a72b197
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-27 20:00:49 +0530

    Update models.py

--- amuse/models.py
+++ amuse/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+from django.utils import timezone
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=True)
