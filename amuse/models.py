d25f11f4c43dcefb23cd834945fef4e05450ff15
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-27 12:55:34 +0530

    Update models.py

--- amuse/models.py
+++ amuse/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
@@ -14,11 +14,3 @@ class LoginAttempt(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     failed_login_attempts = models.IntegerField(default=0)
     last_failed_login_attempt = models.DateTimeField(null=True, blank=True)
-
-class UserGroup(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-
-class UserPermission(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
