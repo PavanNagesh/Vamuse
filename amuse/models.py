09b267d62a477593356e85905783ab66d2885f0c
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-27 13:19:05 +0530

    Update models.py

--- amuse/models.py
+++ amuse/models.py
@@ -1,16 +1,44 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-from django.utils import timezone
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, unique=True)
+    username = models.CharField(max_length=150, unique=True)  # Adding username field
     password = models.CharField(max_length=128)
 
-    def __str__(self):
+    def _str_(self):
         return self.username
+    
+    pass
 
-class LoginAttempt(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    failed_login_attempts = models.IntegerField(default=0)
-    last_failed_login_attempt = models.DateTimeField(null=True, blank=True)
+
+    # Define a custom intermediary model for the groups field
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name='user_set_custom',
+        related_query_name='user_custom',
+        through='UserGroup',
+    )
+
+    # Define a custom intermediary model for the user_permissions field
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='user_set_custom',
+        related_query_name='user_custom',
+        through='UserPermission',
+    )
+
+# Custom intermediary model for the groups field
+class UserGroup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+
+# Custom intermediary model for the user_permissions field
+class UserPermission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
