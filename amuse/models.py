d7bcbc21aa832191d567745e2d72c7fc819c4ad1
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2024-04-28 11:56:37 +0530

    Update models.py

--- amuse/models.py
+++ amuse/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
